314b220747e729d85a1a1d86b39dc8d946c443f5
buck-nicholas <buck.nicholas.c@gmail.com>
2018-07-17 14:06:40 -0500

    Skeleton functions added to Query class to satisfy the needs and errors of the Customer class

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -8,6 +8,57 @@ namespace HumaneSociety
 {
     public static class Query
     {
-        
+        public static void GetClient(string username, string password)
+        {
+
+        }
+        public static void GetUserAdoptionStatus(client)
+        {
+
+        }
+        public static void GetAnimalByID(iD)
+        {
+
+        }
+        public static void Adopt(animal, client)
+        {
+
+        }
+        public static void RetrieveClients()
+        {
+
+        }
+        public static void GetStates()
+        {
+
+        }
+        public static void AddNewClient(firstName, lastName, username, password, email, streetAddress, zipCode, state)
+        {
+
+        }
+        public static void updateClient(client)
+        {
+
+        }
+        public static void UpdateUsername(client)
+        {
+
+        }
+        public static void UpdateEmail(client)
+        {
+
+        }
+        public static void UpdateAddress(client)
+        {
+
+        }
+        public static void UpdateFirstName(client)
+        {
+
+        }
+        public static void UpdateLastName(client)
+        {
+
+        }
     }
 }
